7b30b2184e9fff5569e3d15c7b70ac0a27819731
Teodora Gjini <teodoragjini@yahoo.com>
2022-09-01 00:00:42 +0200

    remove node

--- index.ts
+++ index.ts
@@ -7,18 +7,27 @@ const quotes = [
   {
     id: 1,
     quote: "The purpose of our lives is to be happy.",
-    author: "Dalai Lama",
+    firstName: "Dalai",
+    lastName:"Lama",
+    image:"https://upload.wikimedia.org/wikipedia/commons/5/55/Dalailama1_20121014_4639.jpg",
+    age:"87 Years Old"
+
   },
   {
     id: 2,
     quote: "Get busy living or get busy dying.",
-    author: "Stephen King",
+    firstName: "Stephen",
+    lastName:"King",
+    image:"https://images.gr-assets.com/authors/1362814142p8/3389.jpg",
+    age:"74 Years Old"
   },
   {
     id: 3,
-    quote:
-      "Too many of us are not living our dreams because we are living our fears.",
-    author: "Les Brown",
+    quote:"Too many of us are not living our dreams because we are living our fears.",
+    firstName: "Les",
+    lastName:"Brown",
+    image:"https://cdn.motivationgrid.com/wp-content/uploads/2014/02/les_brown-true.jpg",
+    age:"77 Years Old"
   },
 ];
 
