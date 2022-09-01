72283c9e844d5fe4cf050556736878a02f1aef9a
Teodora Gjini <teodoragjini@yahoo.com>
2022-09-02 01:10:49 +0200

    app.patch

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 const app = express();
-app.use(cors())
-app.use(express.json())
-const port = 3456;let quotes = [
+app.use(cors());
+app.use(express.json());
+const port = 3456;
+
+let quotes = [
   {
     id: 1,
     quote: "The purpose of our lives is to be happy.",
@@ -12,7 +14,7 @@ const port = 3456;let quotes = [
     lastName: "Lama",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/5/55/Dalailama1_20121014_4639.jpg",
-    age: 87  
+    age: 87,
   },
   {
     id: 2,
@@ -20,7 +22,7 @@ const port = 3456;let quotes = [
     firstName: "Stephen",
     lastName: "King",
     image: "https://images.gr-assets.com/authors/1362814142p8/3389.jpg",
-    age: 74
+    age: 74,
   },
   {
     id: 3,
@@ -30,7 +32,7 @@ const port = 3456;let quotes = [
     lastName: "Brown",
     image:
       "https://cdn.motivationgrid.com/wp-content/uploads/2014/02/les_brown-true.jpg",
-    age: 77
+    age: 77,
   },
 ];
 
@@ -46,56 +48,87 @@ app.get("/quotes/:id", (req, res) => {
 });
 
 app.post("/quotes", (req, res) => {
-  let errors: string[] = []
+  let errors: string[] = [];
 
-  if(typeof req.body.quote !== 'string'){
-    errors.push('Quote not given or not a string')
+  if (typeof req.body.quote !== "string") {
+    errors.push("Quote not given or not a string");
   }
 
-  if(typeof req.body.firstName !== 'string'){
-    errors.push('First Name not found or not a string')
+  if (typeof req.body.firstName !== "string") {
+    errors.push("First Name not found or not a string");
   }
 
-  if(typeof req.body.lastName !== 'string'){
-    errors.push('Last Name not found or not a string')
+  if (typeof req.body.lastName !== "string") {
+    errors.push("Last Name not found or not a string");
   }
 
-  if(typeof req.body.image !== 'string'){
-    errors.push('Image not found or not a string')
+  if (typeof req.body.image !== "string") {
+    errors.push("Image not found or not a string");
   }
 
-  if(typeof req.body.age !== 'number'){
-    errors.push('Age not found or not a number')
+  if (typeof req.body.age !== "number") {
+    errors.push("Age not found or not a number");
   }
 
-  if (errors.length === 0){
-  const newQuotes = {
-    id: quotes[quotes.length - 1].id + 1,
-    quote: req.body.quote,
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    image: req.body.image,
-    age: req.body.age,
-  };
+  if (errors.length === 0) {
+    const newQuotes = {
+      id: quotes[quotes.length - 1].id + 1,
+      quote: req.body.quote,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      image: req.body.image,
+      age: req.body.age,
+    };
 
-  quotes.push(newQuotes);
-  res.send(newQuotes);
-} else{
-  res.status(404).send({errors})
-}
+    quotes.push(newQuotes);
+    res.send(newQuotes);
+  } else {
+    res.status(404).send({ errors });
+  }
 });
 
-app.delete("/quotes/:id",(req, res) => {
-  const id = Number(req.params.id)
-  const indexToDelete = quotes.findIndex(quote => quote.id === id)
+app.delete("/quotes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const indexToDelete = quotes.findIndex((quote) => quote.id === id);
 
-  if (indexToDelete  > -1){
-    quotes = quotes.filter(quote => quote.id !== id)
-    res.send('Quote delete successfully.')
+  if (indexToDelete > -1) {
+    quotes = quotes.filter((quote) => quote.id !== id);
+    res.send("Quote delete successfully.");
   } else {
-    res.status(404).send('Quote not found.')
+    res.status(404).send("Quote not found.");
   }
-})
+});
+
+app.patch("/quotes/:id", (req, res) => {
+  let id = Number(req.params.id);
+  let match = quotes.find((quote) => quote.id === id);
+
+  if (match) {
+    if (req.body.quote) {
+      match.quote = req.body.quote;
+    }
+
+    if (req.body.firstName) {
+      match.firstName = req.body.firstName;
+    }
+
+    if (req.body.lastName) {
+      match.lastName = req.body.lastName;
+    }
+
+    if (req.body.image) {
+      match.image = req.body.image;
+    }
+
+    if (req.body.age) {
+      match.age = req.body.age;
+    }
+
+    res.send(match);
+  } else {
+    res.status(404).send("Quote not found");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Yeyy! http://localhost:${port}`);
