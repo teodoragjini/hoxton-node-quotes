2766ac906f47bfb0084d58ee7ebc3df0c291e920
Teodora Gjini <teodoragjini@yahoo.com>
2022-09-06 15:42:45 +0200

    changes

--- index.ts
+++ index.ts
@@ -1,40 +1,42 @@
 import express from "express";
 import cors from "cors";
+import Database from "better-sqlite3";
+const db = Database('./db/data.db',{ verbose: console.log })
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 const port = 3456;
 
-let quotes = [
-  {
-    id: 1,
-    quote: "The purpose of our lives is to be happy.",
-    firstName: "Dalai",
-    lastName: "Lama",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/5/55/Dalailama1_20121014_4639.jpg",
-    age: 87,
-  },
-  {
-    id: 2,
-    quote: "Get busy living or get busy dying.",
-    firstName: "Stephen",
-    lastName: "King",
-    image: "https://images.gr-assets.com/authors/1362814142p8/3389.jpg",
-    age: 74,
-  },
-  {
-    id: 3,
-    quote:
-      "Too many of us are not living our dreams because we are living our fears.",
-    firstName: "Les",
-    lastName: "Brown",
-    image:
-      "https://cdn.motivationgrid.com/wp-content/uploads/2014/02/les_brown-true.jpg",
-    age: 77,
-  },
-];
+// let quotes = [
+//   {
+//     id: 1,
+//     quote: "The purpose of our lives is to be happy.",
+//     firstName: "Dalai",
+//     lastName: "Lama",
+//     image:
+//       "https://upload.wikimedia.org/wikipedia/commons/5/55/Dalailama1_20121014_4639.jpg",
+//     age: 87,
+//   },
+//   {
+//     id: 2,
+//     quote: "Get busy living or get busy dying.",
+//     firstName: "Stephen",
+//     lastName: "King",
+//     image: "https://images.gr-assets.com/authors/1362814142p8/3389.jpg",
+//     age: 74,
+//   },
+//   {
+//     id: 3,
+//     quote:
+//       "Too many of us are not living our dreams because we are living our fears.",
+//     firstName: "Les",
+//     lastName: "Brown",
+//     image:
+//       "https://cdn.motivationgrid.com/wp-content/uploads/2014/02/les_brown-true.jpg",
+//     age: 77,
+//   },
+// ];
 
 app.get("/quotes", (req, res) => {
   res.send(quotes);
